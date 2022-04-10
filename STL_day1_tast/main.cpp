e3e9b9c31df1b2dcf21f109565e9d47c7e57ca0e
Kyoungyoon Park <pky8903@gmail.com>
2022-04-11 07:42:59 +0900

    Initial Commit

---
+++ STL_day1_tast/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  STL_day1_tast
+//
+//  Created by Kyoungyoon Park on 2022/04/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
